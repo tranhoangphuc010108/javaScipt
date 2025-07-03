c1acdfcd21c41731c01684185bb8b95ff04aeaa2
tranhoangphuc010108 <tranhoangphuc010108@gmail.com>
2025-07-03 22:49:19 +0700

    Mô tả ngắn về lần thay đổi, ví dụ: thêm giao diện giỏ hàng

---
+++ ss7_components/Greeting.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    props: {
+        name: {
+        type:String,
+        required: true,
+        default: 'Guest',
+        },
+        age: {
+type: Number,
+required: true,
+default: 0,
+        },
+    }
+}
+</script>
+
+
+<template>
+    <div>
+        <h1>Hello {{ name }}</h1>
+ <p>You are {{ age }} years old</p>
+ </div>
+</template>
+<style scoped>
+</style>
